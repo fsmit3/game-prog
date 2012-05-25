5437be625297709fcd28a5e0ca1d50a71b7901cd
Sander Latour <latour@uva.nl>
2012-05-25 17:21:21 +0200

    Fixed typo

--- code/HumanAgent.js
+++ code/HumanAgent.js
@@ -2,7 +2,7 @@ function HumanAgent(name, img, action_tree){
   var _self = inherit(this, new Agent(name, img, action_tree));
   var _parent = _self._parent;
   
-  this.informDesire = function(actions){
+  this.informDesires = function(actions){
     //Human interaction
     requestApproval(this.getGameController(), actions);
   }
