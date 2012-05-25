8139cc93d8d4037eaee0ef081a98a61fd0979cec
Frank Smit <frank.smit@student.uva.nl>
2012-05-25 17:13:28 +0200

    added small action tree

--- code/ActionTree.js
+++ code/ActionTree.js
@@ -4,11 +4,22 @@ function ActionTree(){
   this.getRoot = function(){ return _root; };
 
   this.init = function(){
-    _root = new ActionNode("Root",{});
-    var node_1 = new ActionNode("Food", {}, _root);
-    var node_11 = new ActionNode("Create more farm land", {}, _root);
-    var node_2 = new ActionNode("Safety", {}, root);
-    var node_21 = new ActionNode("Create more guard towers", {}, node2);
+    _root = new ActionNode("Root",{"food": 2, "safety": 3});
+    // depth = 1
+    var node_11 = new ActionNode("More food", {"food": 5, "safety": -1}, _root);
+    var node_12 = new ActionNode("More police", {"food": -3, "safety": 4}, _root);
+    
+    // depth = 2
+    var node_21 = new ActionNode("More farms", {"food": 5, "safety": -1}, node_11);
+    var node_22 = new ActionNode("More police stations", {"food": -1, "safety": 2}, node_12);
+    var node_23 = new ActionNode("Train police", {"food": -2, "safety": 2}, node_12);
+    
+    // depth = 3
+    var node_31 = new ActionNode("Clear lands", {"food": 2, "safety": 0}, node_21);
+    var node_32 = new ActionNode("Build farms", {"food": 3, "safety": -1}, node_21);
+    var node_33 = new ActionNode("Build police schools", {"food": -1, "safety": 1}, node_23);
+    var node_34 = new ActionNode("Hire police teachers", {"food": -1, "safety": 1}, node_23);
+
   }
 
   this.findHighestPossibleNode = function(value){
